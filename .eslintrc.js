b4b4891edf3225cd290f4645e5f7220b688456e7
Sarath VS <sarathvss65@gmail.com>
2021-05-08 15:23:32 +0530

    Add deps

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 110,
+      },
+    ],
+  },
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import logo from './logo.svg'
+import './App.css'
+
+const Center = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Center>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
